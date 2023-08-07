e3fca391540b510790f9a78738e135abf3741168
Acrylate1410 <quangnm1410@gmail.com>
2023-08-07 17:31:03 +0700

    less div

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
                   {["gói/hộp", "gram/gói"].map(i =>
                     <div className='pt-4 w-1/2' key={i}>
                       <div className='border-l-2 pl-4 border-l-sky-400 text-sky-400 text-3xl font-bold w-1/2 mx-auto'>30</div>
-                      <div className='mt-3 w-1/2 mx-auto text-lg'>{i}</div>
+                      <p className='mt-3 w-1/2 mx-auto text-lg'>{i}</p>
                     </div>
                   )}
               </div>
